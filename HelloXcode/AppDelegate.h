ca14430f3f68a412650a8f83d4321096a7ccde12
Ron Johnston <ron@server.johnston.private>
2014-06-02 10:06:13 -0500

    Initial Commit

---
+++ HelloXcode/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  HelloXcode
+//
+//  Created by Ron Johnston on 6/2/14.
+//  Copyright (c) 2014 Ron Johnston. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
